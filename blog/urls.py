7a8187e1760ae342bf0353404463611db4fdf436
shubham <adeshmukh353@gmail.com>
2020-12-04 00:39:13 +0530

    created url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .import views
+
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', views.home,name="home"),
+    path('blog/', views.blog,name="blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import request,HttpResponse
 # Create your views here.
+def home(request):
+    return HttpResponse("you are in home")
+
+def blog(request):
+    return HttpResponse("you are in blog")
