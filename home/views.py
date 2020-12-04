4a81fafe7aa57c53acf4e5b0cc1526f02c960ea2
shubham <adeshmukh353@gmail.com>
2020-12-04 16:02:03 +0530

    using django's message framework and displaying alerts on contact page

--- home/views.py
+++ home/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import request,HttpResponse
 # Create your views here.
 from .models import Contact
+from django.contrib import messages
 
 def home(request):
     return render(request,'home/home.html')
 
 def contact(request):
+    messages.error(request,'oops!!!')
     if request.method=="POST":
         name=request.POST.get('name','')
         email=request.POST.get('email','')
@@ -15,6 +17,7 @@ def contact(request):
 
         ContactObj=Contact(name=name,email=email,phone=phone,content=content)
         ContactObj.save()
+        messages.success(request, 'Feedback Sent!!!')
     return render(request,"home/contact.html")      
 
 def about(request):
