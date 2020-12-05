52e38ea644d7044af3be679b3740bba117f7aa92
shubham <adeshmukh353@gmail.com>
2020-12-05 21:12:13 +0530

    improving blogHome page

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,11 @@ class Post(models.Model):
     sno=models.AutoField(primary_key=True)
     title=models.CharField(max_length=200)
     author=models.CharField(max_length=200)
+    image = models.ImageField(upload_to="blog/images",default=" ")
     content=models.TextField()
     slug=models.CharField(max_length=130,default="")
     timestamp=models.DateTimeField(blank=True)
 
     def __str__(self):
-        return(f"title by  {self.author}")
+        return(f"title by  {self.title}")
     
--- iCoder/settings.py
+++ iCoder/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 
 MESSAGE_TAGS={
     messages.ERROR:'danger',
