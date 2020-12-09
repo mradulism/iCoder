bb339cf287a3bd1f83fe34187e60300bb596ec7b
shubham <adeshmukh353@gmail.com>
2020-12-09 12:10:30 +0530

    created comments model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
@@ -14,4 +14,12 @@ class Post(models.Model):
 
     def __str__(self):
         return(f"title by  {self.title}")
-    
+
+
+class BlogComment(models.Model):
+    sno = models.AutoField(primary_key=True)
+    comment = models.TextField()
+    user =  models.ForeignKey(User,on_delete=models.CASCADE )
+    post = models.ForeignKey(Post , on_delete=models.CASCADE)
+    parent = models.ForeignKey('self',on_delete=models.CASCADE ,null=True)
+    timestamp = models.DateTimeField()
--- iCoder/urls.py
+++ iCoder/urls.py
@@ -18,6 +18,9 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "iCoder Admin"
+admin.site.site_title = "iCoder Admin Panel"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
